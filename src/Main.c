0e0830909650fa0e0df6d126995edc7aab300b2f
SODABYTES <146698058+SODABYTES@users.noreply.github.com>
2026-01-05 16:10:10 -0500

    Initial commit

---
+++ src/Main.c
@@ -0,0 +1,23 @@
+﻿#include "Main.h"
+
+#if defined NDS
+#include <nds.h>
+#endif
+
+int main(void)
+{
+#if defined NDS
+	consoleDemoInit();
+#endif
+
+	printf("Hello World!\nThis project should compile\nfor both NDS and Windows.\n");
+	
+#if defined NDS
+	while (pmMainLoop())
+	{
+		swiWaitForVBlank();
+	}
+#endif
+
+	return 0;
+}
